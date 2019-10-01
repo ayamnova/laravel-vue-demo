4b06833861344590cb7158343c876d36354e40a7
ayamnova <karstenladdy@gmail.com>
2019-09-30 21:00:57 -0500

    Fixed a spelling error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
-		<link href="{{ asset('css/app.css') }}" type="text/css" rel="stylsheet"/>
+		<link href="{{ asset('css/app.css') }}" type="text/css" rel="stylesheet" />
 		<script>window.Laravel = { csrfToken: '{{csrf_token() }}' }</script>
         <title>Laravel</title>
     </head>
